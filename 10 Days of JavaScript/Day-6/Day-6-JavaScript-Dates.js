4e3570f7c28b002b092b2cb92064b84f11d92131
Elida Ceylan <90249750+elidacy@users.noreply.github.com>
2022-11-29 16:01:30 +0300

    Delete Day-6-JavaScript-Dates.js

--- 10 Days of JavaScript/Day-6/Day-6-JavaScript-Dates.js
+++
@@ -1,8 +0,0 @@
-
-// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-function getDayName(dateString) {
-    let dayName = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    // Write your code here
-    let day = new Date(dateString).getDay()
-    return dayName[day];
-}
